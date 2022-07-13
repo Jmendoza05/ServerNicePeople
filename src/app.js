70462ae040f9babb1f62761a0f68e1d35c00acaf
Juan Carlos Mendoza Torres <72877585+Jmendoza05@users.noreply.github.com>
2022-07-12 22:18:42 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const morgan = require ('morgan');
 const cors = require('cors');
-
+const conectarDB = require('./database');
 const app = express();
 app.set('port',process.env.PORT || 4000);
 app.use(express.json());
 app.use(cors());
-
-///app.use(morgan('dev'));
-
+conectarDB();
 app.use('/api/contenidos',require('./routes/contenidos.routes'));
-
+app.listen(app.get('port'),() =>{
+    console.log("Server Success on port",app.get('port'));
+});
 module.exports = app;
